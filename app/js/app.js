d4b0d5952332fa04ae0d35c90b8b1cd85c7420f9
Maciej Grula <maciej.grula@execon.pl>
2023-08-06 11:05:51 +0200

    fix [T-01]

--- app/js/app.js
+++ app/js/app.js
@@ -194,7 +194,7 @@ class AppDemo {
                 project.tasks.sort((a,b)=>{return b.score-a.score});
                 project.work = moment.duration(project.duration).humanize()
                 project.id = this._ellipsis(project.id)                
-                project.hide = Object.keys(that.model.messages).length>0?that.model.messages.users.find((item)=>{return item.id == user.id}).projects.find((item)=>{return item.id == project.id}).hide:false
+                project.hide = Object.keys(that.model.messages).length>0&&that.model.messages.users.length>0?that.model.messages.users.find((item)=>{return item.id == user.id}).projects.find((item)=>{return item.id == project.id}).hide:false
                 project.tasks.forEach((task)=>{
                     task.work = moment.duration(task.duration).humanize()
                     // task.id = task.id?task.id:"Unnamed Task"
