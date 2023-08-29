03b838b33a3841d930913345901dbd0708fc908f
Maciej Grula <maciej.grula@execon.pl>
2023-08-29 13:21:20 +0200

    change default url

--- logic/constants.js
+++ logic/constants.js
@@ -5,6 +5,6 @@ module.exports = {
         SYNC_INTERVAL_MS: 1000*60*2 // every 2 minutes
     },
     JOIN: {
-        DEFAULT_URLS: ["http://localhost:7071/auth/join"],
+        DEFAULT_URLS: ["https://devjam-lab.azurewebsites.net/auth/join"],
     }
 }
