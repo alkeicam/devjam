90063e2ad54ad5dadb634234f4f8691b75f27cc7
Maciej Grula <maciej.grula@execon.pl>
2023-09-24 15:17:49 +0200

    set app logs path

--- main.js
+++ main.js
@@ -107,5 +107,6 @@ app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
 })
 
+app.setAppLogsPath();
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
