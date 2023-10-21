af43c492fb3d5963371eb9aac2b5b441d99a50fc
Maciej Grula <maciej.grula@execon.pl>
2023-10-21 08:26:19 +0200

    instructions for windows

--- app/js/onboardingModalController.js
+++ app/js/onboardingModalController.js
@@ -20,13 +20,14 @@ class OnboardingModalController {
                 }
             },
             item: {},
+            command: "",
             howMany: 0,
             current: 0,
             notify: {
                 errors: []
             }
         }
-        this.emitter.on("showModal:onboarding",(item)=>{
+        this.emitter.on("showModal:onboarding",async (item)=>{
             this.model.notify.errors = [];
             this.model.form = {
                 f1: {
@@ -44,8 +45,70 @@ class OnboardingModalController {
             }
             this.model.display = true;
             this.model.item = item
+            this.model.command = await this.generateCommand(item);
+
         })
         
+    }
+
+    async generateCommand(item){
+        const postCommitCommand = `
+#!/bin/sh
+GIT_LOG=\`git log --stat -1 HEAD | base64\`
+GIT_DIFF=\`git show --unified | base64\`
+REMOTE=\`git config --get remote.origin.url\`
+LOCAL=\`git rev-parse --show-toplevel\`
+ACCOUNT="${item.id}"
+USER="${item.invitee.email}"
+PROJECT="${item.project.id}"
+
+FINAL_REMOTE=""
+
+if [ -n "$REMOTE" ]; then
+    FINAL_REMOTE=$REMOTE
+else
+    FINAL_REMOTE=$LOCAL
+fi
+
+POST_BODY="{\"gitlog\":\"$GIT_LOG\",\"oper\":\"commit\",\"remote\":\"$FINAL_REMOTE\",\"diff\":\"$GIT_DIFF\",\"account\":\"$ACCOUNT\",\"user\":\"$USER\",\"project\":\"$PROJECT\"}"
+curl -S -s -H "Content-Type: application/json" -d "$POST_BODY" http://localhost:5001/v1/hooks/commit
+
+exit 0
+
+        `
+
+        const prePushCommand = `
+#!/bin/sh
+
+GIT_LOG=\`git log --stat -1 HEAD | base64\`
+REMOTE=\`git config --get remote.origin.url\`
+LOCAL=\`git rev-parse --show-toplevel\`
+ACCOUNT="${item.id}"
+USER="${item.invitee.email}"
+PROJECT="${item.project.id}"
+
+FINAL_REMOTE=""
+
+if [ -n "$REMOTE" ]; then
+    FINAL_REMOTE=$REMOTE
+else
+    FINAL_REMOTE=$LOCAL
+fi
+
+POST_BODY="{\"gitlog\":\"$GIT_LOG\",\"oper\":\"push\",\"remote\":\"$FINAL_REMOTE\",\"account\":\"$ACCOUNT\",\"user\":\"$USER\",\"project\":\"$PROJECT\"}"
+
+curl -S -s -H "Content-Type: application/json" -d "$POST_BODY" http://localhost:5001/v1/hooks/push
+
+exit 0  
+
+        `
+
+        const command = 
+        `
+echo '${btoa(postCommitCommand)}' | base64 --decode > .git/hooks/post-commit && chmod +x .git/hooks/post-commit && echo '${btoa(prePushCommand)}' | base64 --decode > .git/hooks/pre-push && chmod +x .git/hooks/pre-push
+        `
+
+        return command;
     }
 
     static async getInstance(emitter){
